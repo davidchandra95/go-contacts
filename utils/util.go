2d60032fe6be76bfac0ee91d649b39314627b611
davidchandra95 <davidchandra95@yahoo.com>
2018-10-22 21:13:49 +0700

    dude

--- utils/util.go
+++ utils/util.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
-func Message(status bool, message string) (map[string]interface{}) {
-	return map[string]interface{} {"status" : status, "message": message}
+func Message(status bool, message string) map[string]interface{} {
+	return map[string]interface{}{"status": status, "message": message}
 }
 
-func Respond(w http.ResponseWriter, data map[string] interface{}) {
+func Respond(w http.ResponseWriter, data map[string]interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
 }
